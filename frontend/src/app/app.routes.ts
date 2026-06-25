1e41600e209b43a5037767dbc6047c676588ca85
ahmed eltyb <ahmedeltyb17@gmail.com>
2026-06-25 20:15:02 +0300

    Remove frontend from repository

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { DASHBOARD_ROUTES } from './dashboard/dashboard.routes';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  ...DASHBOARD_ROUTES,
-];
